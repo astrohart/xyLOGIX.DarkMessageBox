3b5bc56aa7f51ee7ac270beda4f35aa10e67865d
Brian Hart <brian.hart@xylogix.net>
2025-04-28 16:27:42 -0600

    Add project files.

---
+++ xyLOGIX.DarkMessageBox/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xyLOGIX.DarkMessageBox
+{
+    public class Class1
+    {
+    }
+}
