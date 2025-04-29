ff1178b2edf851c2ee17b7bddd6742b5db8c1d34
Brian Hart <brian.hart@xylogix.net>
2025-04-29 13:56:01 -0600

    Updating BartSimpsonTheme and CowabungaTheme files
    - Added static and private constructors to prevent direct instantiation.
    - Introduced a static `Instance` property for singleton access.
    - Updated button text in `CowabungaTheme.cs` for thematic consistency.

--- xyLOGIX.DarkMessageBox/Themes/BartSimpsonTheme.cs
+++ xyLOGIX.DarkMessageBox/Themes/BartSimpsonTheme.cs
@@ -1,15 +1,40 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 using System.Windows.Forms;
 using xyLOGIX.DarkMessageBox.Interfaces;
 
 namespace xyLOGIX.DarkMessageBox.Themes
 {
     /// <summary>
-    /// A fun "Bart Simpson" style theme for <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBox" />
+    /// A fun "Bart Simpson" style theme for
+    /// <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBox" />
     /// dialogs.
     /// </summary>
     public sealed class BartSimpsonTheme : IDarkMessageBoxTheme
     {
+        /// <summary>
+        /// Empty, <c>static</c> constructor to prohibit direct allocation of this class.
+        /// </summary>
+        static BartSimpsonTheme() { }
+
+        /// <summary>
+        /// Empty, <c>private</c> constructor to prohibit direct allocation of this class.
+        /// </summary>
+        private BartSimpsonTheme() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that implements the
+        /// <see cref="T:xyLOGIX.DarkMessageBox.Interfaces.IDarkMessageBoxTheme" />
+        /// interface.
+        /// </summary>
+        public static IDarkMessageBoxTheme Instance
+        {
+            [DebuggerStepThrough] get;
+        } = new BartSimpsonTheme();
+
+        /// <summary>
+        /// Applies the theme to <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBoxMetrics" />.
+        /// </summary>
         public void Apply()
         {
             DarkMessageBoxMetrics.FormBackgroundColor =
--- xyLOGIX.DarkMessageBox/Themes/CowabungaTheme.cs
+++ xyLOGIX.DarkMessageBox/Themes/CowabungaTheme.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 using System.Windows.Forms;
 using xyLOGIX.DarkMessageBox.Interfaces;
 
@@ -9,6 +10,29 @@ namespace xyLOGIX.DarkMessageBox.Themes
     /// </summary>
     public sealed class CowabungaTheme : IDarkMessageBoxTheme
     {
+        /// <summary>
+        /// Empty, <c>static</c> constructor to prohibit direct allocation of this class.
+        /// </summary>
+        static CowabungaTheme() { }
+
+        /// <summary>
+        /// Empty, <c>private</c> constructor to prohibit direct allocation of this class.
+        /// </summary>
+        private CowabungaTheme() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that implements the
+        /// <see cref="T:xyLOGIX.DarkMessageBox.Interfaces.IDarkMessageBoxTheme" />
+        /// interface.
+        /// </summary>
+        public static IDarkMessageBoxTheme Instance
+        {
+            [DebuggerStepThrough] get;
+        } = new CowabungaTheme();
+
+        /// <summary>
+        /// Applies the theme to <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBoxMetrics" />.
+        /// </summary>
         public void Apply()
         {
             DarkMessageBoxMetrics.FormBackgroundColor =
@@ -28,12 +52,12 @@ namespace xyLOGIX.DarkMessageBox.Themes
 
             DarkMessageBoxMetrics.ButtonTexts[DialogResult.OK] = "&Cowabunga!";
             DarkMessageBoxMetrics.ButtonTexts[DialogResult.Cancel] =
-                "&No Way, Dude!";
+                "&Let's quit while we're ahead!";
             DarkMessageBoxMetrics.ButtonTexts[DialogResult.Yes] = "&Radical!";
             DarkMessageBoxMetrics.ButtonTexts[DialogResult.No] = "&Bogus!";
             DarkMessageBoxMetrics.ButtonTexts[DialogResult.Abort] = "&Bail!";
             DarkMessageBoxMetrics.ButtonTexts[DialogResult.Retry] =
-                "&Try Again!";
+                "Can &we try that again?";
             DarkMessageBoxMetrics.ButtonTexts[DialogResult.Ignore] =
                 "&Shell Shock!";
 
