21e1f168e378097f8400b339cbac6cc4c513221b
Brian Hart <brian.hart@xylogix.net>
2025-04-29 15:07:45 -0600

    Removing Apply method in ProfessionalDarkTheme.cs
    - Removed implementation of the `Apply` method.
    - Added a call to `DarkMessageBoxMetrics.Reset()`.
    - Removed unused using directives for `System.Diagnostics`, `System.Drawing`, and `System.Windows.Forms`.
    - Added summary comment for the `Apply` method.

--- xyLOGIX.DarkMessageBox/Themes/ProfessionalDarkTheme.cs
+++ xyLOGIX.DarkMessageBox/Themes/ProfessionalDarkTheme.cs
@@ -1,6 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Drawing;
-using System.Windows.Forms;
 using xyLOGIX.DarkMessageBox.Interfaces;
 
 namespace xyLOGIX.DarkMessageBox.Themes
@@ -31,37 +29,10 @@ namespace xyLOGIX.DarkMessageBox.Themes
             [DebuggerStepThrough] get;
         } = new ProfessionalDarkTheme();
 
+        /// <summary>
+        /// Applies the theme to <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBoxMetrics" />.
+        /// </summary>
         public void Apply()
-        {
-            DarkMessageBoxMetrics.FormBackgroundColor =
-                Color.FromArgb(32, 32, 32);
-            DarkMessageBoxMetrics.FooterBackgroundColor =
-                Color.FromArgb(45, 45, 45);
-            DarkMessageBoxMetrics.ButtonBackgroundColor =
-                Color.FromArgb(50, 50, 50);
-            DarkMessageBoxMetrics.ButtonTextColor = Color.White;
-            DarkMessageBoxMetrics.ButtonBorderColor =
-                Color.FromArgb(70, 70, 70);
-
-            DarkMessageBoxMetrics.HighlightDefaultButtonBackground = false;
-            DarkMessageBoxMetrics.DefaultButtonBackgroundColor =
-                Color.FromArgb(60, 60, 90);
-            DarkMessageBoxMetrics.DefaultButtonBorderColor =
-                Color.FromArgb(180, 200, 255);
-            DarkMessageBoxMetrics.DefaultButtonBorderThickness = 2;
-
-            DarkMessageBoxMetrics.TitleBarIsLight = false;
-
-            DarkMessageBoxMetrics.ButtonTexts[DialogResult.OK] = "OK";
-            DarkMessageBoxMetrics.ButtonTexts[DialogResult.Cancel] = "Cancel";
-            DarkMessageBoxMetrics.ButtonTexts[DialogResult.Yes] = "&Yes";
-            DarkMessageBoxMetrics.ButtonTexts[DialogResult.No] = "&No";
-            DarkMessageBoxMetrics.ButtonTexts[DialogResult.Abort] = "&Abort";
-            DarkMessageBoxMetrics.ButtonTexts[DialogResult.Retry] = "&Retry";
-            DarkMessageBoxMetrics.ButtonTexts[DialogResult.Ignore] = "&Ignore";
-
-            DarkMessageBoxMetrics.WindowIcon = null;
-            DarkMessageBoxMetrics.MessageBodyIcon = null;
-        }
+            => DarkMessageBoxMetrics.Reset();
     }
 }
