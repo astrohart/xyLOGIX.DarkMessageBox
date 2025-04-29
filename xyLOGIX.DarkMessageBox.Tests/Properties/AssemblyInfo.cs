5e9280ef4b53b9edfa746ce107f66794536c0d84
Brian Hart <brian.hart@xylogix.net>
2025-04-29 13:54:23 -0600

    Update AssemblyInfo.cs
    - Add assembly attributes for metadata including title, description, company, product, copyright, and version.
    - Set ComVisible attribute to false to hide types from COM components.
    - Specify GUID for typelib to indicate non-COM exposure.

---
+++ xyLOGIX.DarkMessageBox.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX.DarkMessageBox.Tests")]
+[assembly: AssemblyDescription("Unit-testing library for the DarkMessageBox functionality.  This particular class library makes use of NUnit.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("Unit Tests Library for the xyLOGIX Dark-Themed and Skinnable MessageBox Replacement Component")]
+[assembly: AssemblyCopyright("Copyright © xyLOGIX, LLC 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e1bcc23-9769-4cb3-9dbb-11f3b2653873")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
