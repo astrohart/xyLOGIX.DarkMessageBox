2035db910d40eb76946e5e130921ac2358147e41
Brian Hart <brian.hart@xylogix.net>
2025-04-29 18:44:13 -0600

    Update IDarkMessageBoxTheme.cs
    * Reformatted the source file in accordance with rules that are defined in CodeMaid and ReSharper.

--- xyLOGIX.DarkMessageBox/Interfaces/IDarkMessageBoxTheme.cs
+++ xyLOGIX.DarkMessageBox/Interfaces/IDarkMessageBoxTheme.cs
@@ -1,12 +1,14 @@
 ﻿namespace xyLOGIX.DarkMessageBox.Interfaces
 {
     /// <summary>
-    /// Defines a contract for applying a visual theme to <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBox" /> dialogs.
+    /// Defines a contract for applying a visual theme to
+    /// <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBox" /> dialogs.
     /// </summary>
     public interface IDarkMessageBoxTheme
     {
         /// <summary>
-        /// Applies the theme to <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBoxMetrics" />.
+        /// Applies the theme to
+        /// <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBoxMetrics" />.
         /// </summary>
         void Apply();
     }
