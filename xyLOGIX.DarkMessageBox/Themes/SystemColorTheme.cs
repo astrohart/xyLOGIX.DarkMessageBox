7f3b1bf92ca17e0979e7169c54e399002705b168
Brian Hart <brian.hart@xylogix.net>
2025-04-30 19:27:10 -0600

    Create SystemColorTheme.cs
    - Introduce `SystemColorTheme` class in the
      `xyLOGIX.DarkMessageBox.Themes` namespace.
    - Implement `IDarkMessageBoxTheme` interface.
    - Provide a static instance of the class.
    - Include static and protected constructors to prevent
      direct instantiation.
    - Define `Apply` method to set `DarkMessageBoxMetrics`
      properties using system colors from `SystemColors`.

---
+++ xyLOGIX.DarkMessageBox/Themes/SystemColorTheme.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using xyLOGIX.DarkMessageBox.Interfaces;
+
+namespace xyLOGIX.DarkMessageBox.Themes
+{
+    /// <summary>
+    /// A theme that styles <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBox" />
+    /// according to the value(s) in the <see cref="T:System.Drawing.SystemColors" />
+    /// class.
+    /// </summary>
+    public class SystemColorTheme : IDarkMessageBoxTheme
+    {
+        /// <summary>
+        /// Empty, static constructor to prohibit direct allocation of this class.
+        /// </summary>
+        static SystemColorTheme() { }
+
+        /// <summary>
+        /// Empty, protected constructor to prohibit direct allocation of this class.
+        /// </summary>
+        protected SystemColorTheme() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that implements the
+        /// <see cref="T:xyLOGIX.DarkMessageBox.Interfaces.IDarkMessageBoxTheme" />
+        /// interface.
+        /// </summary>
+        public static IDarkMessageBoxTheme Instance
+        {
+            [DebuggerStepThrough] get;
+        } = new SystemColorTheme();
+
+        /// <summary>
+        /// Applies the theme to
+        /// <see cref="T:xyLOGIX.DarkMessageBox.DarkMessageBoxMetrics" />.
+        /// </summary>
+        public void Apply()
+        {
+            DarkMessageBoxMetrics.FormBackgroundColor =
+                SystemColors.Window; // typical window background
+            DarkMessageBoxMetrics.FooterBackgroundColor =
+                SystemColors.ControlLight; // one shade lighter than ButtonFace
+            DarkMessageBoxMetrics.FlatButtons = false;
+
+            DarkMessageBoxMetrics.ButtonBackgroundColor =
+                SystemColors.ButtonFace;
+            DarkMessageBoxMetrics.ButtonBorderColor = SystemColors.ButtonShadow;
+            DarkMessageBoxMetrics.ButtonTextColor = SystemColors.ControlText;
+
+            DarkMessageBoxMetrics.HighlightDefaultButtonBackground = false;
+            DarkMessageBoxMetrics.DefaultButtonBorderColor =
+                SystemColors.Highlight;
+            DarkMessageBoxMetrics.DefaultButtonBorderThickness = 2;
+
+            DarkMessageBoxMetrics.MessageTextColor = SystemColors.ControlText;
+
+            DarkMessageBoxMetrics.TitleBarIsLight = true;
+        }
+    }
+}
