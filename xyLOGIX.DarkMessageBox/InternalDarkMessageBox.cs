0690de25f5d5a08d53b42b9bf715fcdf36a05efe
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-08-08 12:38:09 -0600

    Update XML docs for event handlers

--- xyLOGIX.DarkMessageBox/InternalDarkMessageBox.cs
+++ xyLOGIX.DarkMessageBox/InternalDarkMessageBox.cs
@@ -960,7 +960,7 @@ namespace xyLOGIX.DarkMessageBox
 
         /// <summary>Raises the <see cref="E:System.Windows.Forms.Form.Shown" /> event.</summary>
         /// <param name="e">
-        /// A <see cref="T:System.EventArgs" /> that contains the event
+        /// An <see cref="T:System.EventArgs" /> that contains the event
         /// data.
         /// </param>
         protected override void OnShown([NotLogged] EventArgs e)
@@ -989,7 +989,7 @@ namespace xyLOGIX.DarkMessageBox
         /// event.
         /// </param>
         /// <param name="e">
-        /// A <see cref="T:System.EventArgs" /> that contains the event
+        /// An <see cref="T:System.EventArgs" /> that contains the event
         /// data.
         /// </param>
         /// <remarks>
